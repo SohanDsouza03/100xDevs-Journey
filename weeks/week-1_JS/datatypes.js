766647164e0677a6d42e558a4f9c10b93f2dbb61
SohanDsouza03 <sohandsouza03@gmail.com>
2026-06-04 22:27:03 +0530

    basic datatype

---
+++ weeks/week-1_JS/datatypes.js
@@ -0,0 +1,9 @@
+let firstname ="Sohan";
+let age= 22;
+let tall= true;
+
+console.log(firstname);
+console.log(age);
+console.log(tall);
+
+console.log("i am "+firstname+" my age is "+age+"i am tall enough"+tall);
