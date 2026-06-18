8046769a1930771640a0679721abb4cadcf3bbd5
SohanDsouza03 <sohandsouza03@gmail.com>
2026-06-18 18:33:07 +0530

    modified datatype

--- weeks/week-1_JS/week-1.2/variables.js
+++ weeks/week-1_JS/week-1.2/variables.js
@@ -1,10 +1,24 @@
 //var
 var a =1;
 console.log(a);
+
+//var using string
+var a="Hello, World!";   //<--- dupicate value repeated
+console.log(a);
+
+
+
 //let  value changes
 let b =1;
 b=2;
-console.log(b);
+// console.log(b);
+
+// let b="hello, world!";
+// console.log(b);                 //<-- cant change duplicate value
 // const  value wont change
-const c=1;
+const c=100;
 console.log(c);
+
+// const c=200;             //<-- cants change onces declared
+// console.log(c);
+
