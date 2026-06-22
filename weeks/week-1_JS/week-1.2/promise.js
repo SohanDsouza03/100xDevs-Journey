06c28f9f06c981202e28581d326feb3c8777614c
SohanDsouza03 <sohandsouza03@gmail.com>
2026-06-22 21:19:39 +0530

    added callback syntax promise(then) syntax and Async/await syntax with 3 simple example

--- weeks/week-1_JS/week-1.2/promise.js
+++ weeks/week-1_JS/week-1.2/promise.js
@@ -23,25 +23,75 @@
 // var a= SohanReadFile();
 // a.then(onDone);
 
+// function SohanAsyncFn(){
+//     let p = new Promise(function(resolve){
+//         //do some async logic here
+//         setTimeout(function(){
+//             resolve("done with async logic");
+//         }, 3000);
+//     });
+
+//     return p;
+// }
+
+// async function main(){
+//     //no callbacks, no .then syntax, just like synchronous code
+//     //SohanAsyncfn().then(function(value){
+//     //    console.log(value);
+//     //});   
+
+//     let value = await SohanAsyncFn();
+//     console.log("hi there 1");
+// }
+// main();
+// console.log("after main");
+
+
+
+
+// using async/await syntax to write asynchronous code in a more synchronous way
 function SohanAsyncFn(){
     let p = new Promise(function(resolve){
         //do some async logic here
-        setTimeout(function(){
-            resolve("done with async logic");
-        }, 3000);
+        resolve("hi there!");
     });
-
     return p;
 }
 
-async function main(){
-    //no callbacks, no .then syntax, just like synchronous code
-    //SohanAsyncfn().then(function(value){
-    //    console.log(value);
-    //});   
-
-    let value = await SohanAsyncFn();
-    console.log("hi there 1");
+async function main(){                    //any function which has await keyword in it should be declared as async function
+    const value = await SohanAsyncFn();  //rather than using .then syntax, we can use await keyword to wait for the promise to resolve and get the value directly
+    console.log(value);
 }
+
 main();
-console.log("after main");
+
+
+
+/// using callback syntax
+function SohanAsyncFnWithCallback(callback){
+    //do some async logic here
+    callback("hi there!");
+}
+async function mainWithCallback(){
+    SohanAsyncFnWithCallback(function(value){
+        console.log(value);
+    });
+}
+mainWithCallback();
+
+// using promise syntax
+function SohanAsyncFnWithPromise(){
+
+    let p = new Promise(function(resolve){
+        //do some async logic here
+        resolve("hi there!");
+    });
+    return p;           
+}
+
+function mainWithPromise(){
+    SohanAsyncFnWithPromise().then(function(value){
+        console.log(value);
+    });
+}
+mainWithPromise();
