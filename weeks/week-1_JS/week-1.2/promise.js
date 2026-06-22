3dc59065af96e4e64f69707cb5b0bd31d3d4bb6d
SohanDsouza03 <sohandsouza03@gmail.com>
2026-06-22 21:04:07 +0530

    await working

--- weeks/week-1_JS/week-1.2/promise.js
+++ weeks/week-1_JS/week-1.2/promise.js
@@ -1,24 +1,47 @@
-const fs = require('fs');
-//filesystem module
+// const fs = require('fs');
+// //filesystem module
 
 
 
-// my own asynchronous function to read a file using fs.readFile
-function SohanReadFile(){
-    return new Promise(function(resolve){
-        console.log("inside promise");
-        fs.readFile('async.txt', 'utf-8', function(err, data){
+// // my own asynchronous function to read a file using fs.readFile
+// function SohanReadFile(){
+//     return new Promise(function(resolve){
+//         console.log("inside promise");
+//         fs.readFile('async.txt', 'utf-8', function(err, data){
             
-                console.log("before resolve");
-                resolve(data);
-            });
-    } )
+//                 console.log("before resolve");
+//                 resolve(data);
+//             });
+//     } )
+// }
+
+// //callback function to call
+// function onDone(data){
+//     console.log(data);
+// }
+
+// var a= SohanReadFile();
+// a.then(onDone);
+
+function SohanAsyncFn(){
+    let p = new Promise(function(resolve){
+        //do some async logic here
+        setTimeout(function(){
+            resolve("done with async logic");
+        }, 3000);
+    });
+
+    return p;
 }
 
-//callback function to call
-function onDone(data){
-    console.log(data);
-}
+async function main(){
+    //no callbacks, no .then syntax, just like synchronous code
+    //SohanAsyncfn().then(function(value){
+    //    console.log(value);
+    //});   
 
-var a= SohanReadFile();
-a.then(onDone);
+    let value = await SohanAsyncFn();
+    console.log("hi there 1");
+}
+main();
+console.log("after main");
