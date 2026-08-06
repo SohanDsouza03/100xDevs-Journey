34ab9877882515c0f2220bfd3b274f867728d90b
SohanDsouza03 <sohandsouza03@gmail.com>
2026-08-06 10:49:07 +0530

    Add largest element program using array traversal

--- weeks/week-1_JS/week-1_revise/biggest_no.js
+++ weeks/week-1_JS/week-1_revise/biggest_no.js
@@ -1,8 +1,11 @@
-const arr=[10,20,30,40,50,60,40,20];
-let big=0;
-for(let i=0;i<arr.length;i++){
-    if(big<arr[i]){
-        big=arr[i];
+const arr = [10,20,30,40,50,60,40,20];
+
+let big = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > big) {
+        big = arr[i];
     }
 }
+
 console.log(big);
