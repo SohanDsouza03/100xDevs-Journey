d7885684cf9066060944dbb387de325e0ff0546d
SohanDsouza03 <sohandsouza03@gmail.com>
2026-09-03 14:49:26 +0530

    Add JWT user middleware

--- assignments/week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ assignments/week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,23 @@
+const jwt = require("jsonwebtoken");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (decoded) {
+            next();
+        } else {
+            res.status(403).json({
+                message: "User authentication failed"
+            });
+        }
+    } catch (error) {
+        res.status(403).json({
+            message: "User authentication failed"
+        });
+    }
 }
 
 module.exports = userMiddleware;
