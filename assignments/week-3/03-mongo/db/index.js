8798898583b998dde51ac2ae1ab02585b1253ebb
SohanDsouza03 <sohandsouza03@gmail.com>
2026-09-03 12:54:30 +0530

    Add MongoDB connection and database schemas

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://sohan0113:WqIcDCWmyWNjja7F@cluster0.l1xjyfl.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:string,
+    password:string
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:string,
+    password:string,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    tile:string,
+    description:string,
+    imageLink:string,
+    price:number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
