57da9216265bf9546db7da3a80c1b08bc617e214
SohanDsouza03 <sohandsouza03@gmail.com>
2026-09-03 14:38:20 +0530

    Connect admin and user routes

--- assignments/week-3/03-mongo/index.js
+++ assignments/week-3/03-mongo/index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const adminRouter = require("./routes/admin")
+const express = require("express");
+
+const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
-// Middleware for parsing request bodies
-app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
+const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use("/admin", adminRouter);
+app.use("/users", userRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
