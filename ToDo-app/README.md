c112cb3b0a8e197c490c1cc63e3b3f2f5d11a286
SohanDsouza03 <sohandsouza03@gmail.com>
2026-09-04 18:44:57 +0530

    Add Todo app

---
+++ ToDo-app/README.md
@@ -0,0 +1,11 @@
+## Todo app
+
+This project contains a simple TODO application 
+It has the following features -
+
+- Anyone can create a TODO
+- Anyone can see their existing todos
+- Anyone can mark as todo as done
+
+//initailize a node project 
+// put a package.json
---
+++ ToDo-app/backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req, res) {
+
+})
+
+app.get("/todos", function(req, res) {
+
+})
+
+app.put("/completed", function(req, res) {
+
+})
+
+// https://shorturl.at/cLR56
+
+// write basic express boilerplate code,
+// with express.json() middleware
